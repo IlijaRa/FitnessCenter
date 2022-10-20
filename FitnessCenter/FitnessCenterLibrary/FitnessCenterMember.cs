2b3779e6e200da4898c2fc9ca2859532af97ee7b
IlijaRa <rakocevicilija7@gmail.com>
2022-10-20 20:46:09 +0200

    added-data-models

---
+++ FitnessCenter/FitnessCenterLibrary/FitnessCenterMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenterLibrary
+{
+    internal class FitnessCenterMember : User
+    {
+        public List<Workout> completed_workouts { get; set; }
+        public List<Workout> requested_workouts { get; set; }
+        public List<Rate> workout_rates { get; set; }
+    }
+}
---
+++ FitnessCenter/FitnessCenterLibrary/Rate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessCenterLibrary
+{
+    internal class Rate
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int CoachId { get; set; }
+        public double Rating { get; set; }
+    }
+}
