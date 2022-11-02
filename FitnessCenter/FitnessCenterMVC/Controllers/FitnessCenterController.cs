71302caf208c48b94564a0bfbdc205f06ba9546e
IlijaRa <rakocevicilija7@gmail.com>
2022-11-02 02:24:57 +0100

    added-crud-for-fitness-center

---
+++ FitnessCenter/FitnessCenterMVC/Controllers/FitnessCenterController.cs
@@ -0,0 +1,104 @@
+﻿using FitnessCenterMVC.Data;
+using FitnessCenterMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using FitnessCenterMVC.ModelMapper;
+
+namespace FitnessCenterMVC.Controllers
+{
+    public class FitnessCenterController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public FitnessCenterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult AddFitnessCenter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFitnessCenter(FitnessCenterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // if we get here, something is wrong
+                return View("Error");
+            }
+
+            var fitnessCenter = FitnessCenterConversions.ConvertToFintessCenter(model);
+            await _context.FitnessCenter.AddAsync(fitnessCenter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditFitnessCenter(int id)
+        {
+            var fitnessCenter = await _context.FitnessCenter.FirstOrDefaultAsync(x => x.Id == id);
+            if (fitnessCenter == null)
+            {
+                // if we get here, something is wrong
+                return View("Error");
+            }
+
+            var fitnessCenterViewModel = FitnessCenterConversions.ConvertToFintessCenterViewModel(fitnessCenter);
+            return View(fitnessCenterViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditFitnessCenter(FitnessCenterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // if we get here, something is wrong
+                return View("Error");
+            }
+
+            var fitnessCenter = await _context.FitnessCenter.FirstOrDefaultAsync(x => x.Id == model.Id);
+
+            fitnessCenter.Title = model.Title;
+            fitnessCenter.PhoneNumber = model.PhoneNumber;
+            fitnessCenter.EmailAddress = model.EmailAddress;
+            fitnessCenter.Street = model.Street;
+            fitnessCenter.Number = model.Number;
+            fitnessCenter.City = model.City;
+            fitnessCenter.ZipCode = model.ZipCode;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFitnessCenters()
+        {
+            var fitnessCenterViewModels = new List<FitnessCenterViewModel>();
+            var fitnessCenters = await _context.FitnessCenter.ToListAsync();
+
+            foreach (var fitnessCenter in fitnessCenters)
+            {
+                fitnessCenterViewModels.Add(FitnessCenterConversions.ConvertToFintessCenterViewModel(fitnessCenter));
+            }
+            return View(fitnessCenterViewModels);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteFitnessCenter(int id)
+        {
+            var fitnessCenter = await _context.FitnessCenter.FirstOrDefaultAsync(x => x.Id == id);
+            if(fitnessCenter == null)
+            {
+                // if we get here, something is wrong
+                return View("Error");
+            }
+
+            _context.FitnessCenter.Remove(fitnessCenter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("GetAllFitnessCenters", "FitnessCenter");
+        }
+    }
+}
---
+++ FitnessCenter/FitnessCenterMVC/Models/FitnessCenterViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FitnessCenterMVC.Models
+{
+    public class FitnessCenterViewModel
+    {
+        public int Id { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You need to provide a title.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "You need to provide a title between 3-50 characters.")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "You need to provide a phone number.")]
+        [MaxLength(11, ErrorMessage = "Phone number cannot be longer than 11 numbers.")]
+        public string PhoneNumber { get; set; }
+
+
+        [Display(Name = "Email address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to provide an email address.")]
+        public string EmailAddress { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You need to provide a street name.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "You need to provide a street name between 1-50 characters.")]
+        public string Street { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You need to provide a street number.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "You need to provide a street number between 1-50 characters.")]
+        public string Number { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You need to provide a city.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "You need to provide a city between 1-50 characters.")]
+        public string City { get; set; }
+
+
+        [DataType(DataType.PostalCode)]
+        [Required(ErrorMessage = "You need to provide postal code.")]
+        public string ZipCode { get; set; }
+    }
+}
