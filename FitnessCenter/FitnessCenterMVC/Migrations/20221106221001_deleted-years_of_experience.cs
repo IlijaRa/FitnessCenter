57315da6ecb4898efb84c66522b2cca05ce0eb61
IlijaRa <rakocevicilija7@gmail.com>
2022-11-06 23:31:11 +0100

    added-migration

---
+++ FitnessCenter/FitnessCenterMVC/Migrations/20221106221001_deleted-years_of_experience.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessCenterMVC.Migrations
+{
+    public partial class deletedyears_of_experience : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YearsOfExperience",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "YearsOfExperience",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
